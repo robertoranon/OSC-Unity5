7fd8a098d047aa36fe19b7e44c10d7022e423269
Roberto Ranon <roberto.ranon@uniud.it>
2016-12-27 19:32:28 +0100

    update for unity 5.2

--- Assets/Scripts/OSCAnimateStandardMaterial.cs
+++ Assets/Scripts/OSCAnimateStandardMaterial.cs
@@ -89,9 +89,9 @@ public class OSCAnimateStandardMaterial : OSCAnimation {
 				case STANDARD_MATERIAL_FIELDS.Emission_Brightness:
 					prevColor = mat.GetColor("_EmissionColor");
 					float h,s,v;
-					Color.RGBToHSV( prevColor, out h, out s, out v);
+					RGBToHSV2( prevColor, out h, out s, out v);
 					v = float.Parse(localMsg.Values[0].ToString());
-					Color newValue = Color.HSVToRGB( h, s, v );
+					Color newValue = HSVToRGB2( h, s, v );
 					mat.SetColor("_EmissionColor", newValue );
 					break;
 
@@ -140,4 +140,131 @@ public class OSCAnimateStandardMaterial : OSCAnimation {
 		}
 		
 	}
+
+	public static Color HSVToRGB2(float H, float S, float V)
+	{
+		if (S == 0f)
+			return new Color(V,V,V);
+		else if (V == 0f)
+			return Color.black;
+		else
+		{
+			Color col = Color.black;
+			float Hval = H * 6f;
+			int sel = Mathf.FloorToInt(Hval);
+			float mod = Hval - sel;
+			float v1 = V * (1f - S);
+			float v2 = V * (1f - S * mod);
+			float v3 = V * (1f - S * (1f - mod));
+			switch (sel + 1)
+			{
+			case 0:
+				col.r = V;
+				col.g = v1;
+				col.b = v2;
+				break;
+			case 1:
+				col.r = V;
+				col.g = v3;
+				col.b = v1;
+				break;
+			case 2:
+				col.r = v2;
+				col.g = V;
+				col.b = v1;
+				break;
+			case 3:
+				col.r = v1;
+				col.g = V;
+				col.b = v3;
+				break;
+			case 4:
+				col.r = v1;
+				col.g = v2;
+				col.b = V;
+				break;
+			case 5:
+				col.r = v3;
+				col.g = v1;
+				col.b = V;
+				break;
+			case 6:
+				col.r = V;
+				col.g = v1;
+				col.b = v2;
+				break;
+			case 7:
+				col.r = V;
+				col.g = v3;
+				col.b = v1;
+				break;
+			}
+			col.r = Mathf.Clamp(col.r, 0f, 1f);
+			col.g = Mathf.Clamp(col.g, 0f, 1f);
+			col.b = Mathf.Clamp(col.b, 0f, 1f);
+			return col;
+		}
+	}
+
+
+	public static void RGBToHSV2(Color rgbColor, out float H, out float S, out float V)
+	{
+		if (rgbColor.b > rgbColor.g && rgbColor.b > rgbColor.r)
+		{
+			RGBToHSVHelper(4f, rgbColor.b, rgbColor.r, rgbColor.g, out H, out S, out V);
+		}
+		else
+		{
+			if (rgbColor.g > rgbColor.r)
+			{
+				RGBToHSVHelper(2f, rgbColor.g, rgbColor.b, rgbColor.r, out H, out S, out V);
+			}
+			else
+			{
+				RGBToHSVHelper(0f, rgbColor.r, rgbColor.g, rgbColor.b, out H, out S, out V);
+			}
+		}
+	}
+
+	private static void RGBToHSVHelper(float offset, float dominantcolor, float colorone, float colortwo, out float H, out float S, out float V)
+	{
+		V = dominantcolor;
+		if (V != 0f)
+		{
+			float num = 0f;
+			if (colorone > colortwo)
+			{
+				num = colortwo;
+			}
+			else
+			{
+				num = colorone;
+			}
+			float num2 = V - num;
+			if (num2 != 0f)
+			{
+				S = num2 / V;
+				H = offset + (colorone - colortwo) / num2;
+			}
+			else
+			{
+				S = 0f;
+				H = offset + (colorone - colortwo);
+			}
+			H /= 6f;
+			if (H < 0f)
+			{
+				H += 1f;
+			}
+		}
+		else
+		{
+			S = 0f;
+			H = 0f;
+		}
+	}
+
+
 }
+
+
