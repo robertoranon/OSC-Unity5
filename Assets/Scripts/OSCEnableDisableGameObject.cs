dca69b355f1421d099d9e6a283185ad76607a3b4
Roberto Ranon <roberto.ranon@uniud.it>
2017-09-28 13:50:29 +0200

    Added script to enable/disable rendering of gameobject (and its children). A OSC value of 0 disables rendering, any positive value enables rendering

---
+++ Assets/Scripts/OSCEnableDisableGameObject.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class OSCEnableDisableGameObject : OSCAnimation {
+
+	
+	// Use this for initialization
+	void Start () {
+
+		
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (newMessage) {
+
+			try
+			{
+
+				int value = int.Parse(localMsg.Values[0].ToString());
+				
+				if (value > 0) {
+
+					MeshRenderer[] allRenderers = this.GetComponentsInChildren<MeshRenderer>();
+					foreach (MeshRenderer r in allRenderers) {
+						r.enabled = false;
+					}
+
+					SkinnedMeshRenderer[] allSkinnedRenderers = this.GetComponentsInChildren<SkinnedMeshRenderer>();
+					foreach (SkinnedMeshRenderer r in allSkinnedRenderers) {
+						r.enabled = false;
+					}
+
+				} else {
+
+					MeshRenderer[] allRenderers = this.GetComponentsInChildren<MeshRenderer>();
+					foreach (MeshRenderer r in allRenderers) {
+						r.enabled = true;
+					}
+
+					SkinnedMeshRenderer[] allSkinnedRenderers = this.GetComponentsInChildren<SkinnedMeshRenderer>();
+					foreach (SkinnedMeshRenderer r in allSkinnedRenderers) {
+						r.enabled = true;
+					}
+					
+				} 
+				
+
+			}
+			catch (System.Exception e)
+			{
+				Debug.Log ("Wrong propertyname, or missing component, or type mismatch between message value and property value");
+			}
+
+			newMessage = false;
+
+		}
+
+	}
+}
